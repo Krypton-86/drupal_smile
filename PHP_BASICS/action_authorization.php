5e01807c5e36c61c051f9b74f527a77c2f7a487f
Petro <mccolaichuk@gmail.com>
2021-09-05 16:36:36 +0300

    Added validations

--- PHP_BASICS/action_authorization.php
+++ PHP_BASICS/action_authorization.php
@@ -1,11 +1,77 @@
 <?php
-
-function get_POST_email ():string {
-  return htmlentities($_POST['email'], ENT_QUOTES);
-}
-//ToDo: Password Hashing
-function get_POST_password ():string {
-  return password_hash($_POST['password'], PASSWORD_DEFAULT);
+class post_class {
+  public $flag = TRUE;
+  function getEmail(): string {
+    $return_var = htmlentities($_POST['email'], ENT_QUOTES);
+    $len=str_len($return_var);
+   if ($len < 5 || $len > 255){
+      $this->flag=FALSE;
+    }
+      return $return_var;
+  }
+  function getPassword(): string {
+    $return_var = htmlentities($_POST['password'], ENT_QUOTES);
+    $len=str_len($return_var);
+    if ($len < 8 || $len > 32){
+      $this->flag=FALSE;
+    }
+    return $return_var;
+  }
+  function getPasswordHash(): string {
+    return password_hash($this->getPassword(), PASSWORD_DEFAULT);
+  }
+  function getFname(): string {
+    $return_var = htmlentities($_POST['fname'], ENT_QUOTES);
+    $len=str_len($return_var);
+    if ($len < 3 || $len > 255){
+      $this->flag=FALSE;
+    }
+    return $return_var;
+  }
+  function getLname(): string {
+    $return_var = htmlentities($_POST['lname'], ENT_QUOTES);
+    $len=str_len($return_var);
+    if ($len < 3 || $len > 255){
+      $this->flag=FALSE;
+    }
+    return $return_var;
+  }
+  function getBirthday(): string {
+    $return_var = htmlentities($_POST['birthday'], ENT_QUOTES);
+    $len=str_len($return_var);
+    if ($len < 8 || $len > 10){
+      $this->flag=FALSE;
+    }
+    return $return_var;
+  }
+  function getCategories(): string {
+    $categories = NULL;
+    foreach ($_POST['categories'] as $category){
+    if ($categories===NULL){
+      $categories = $category;
+    }else
+      $categories = $categories . ", " . htmlentities($category, ENT_QUOTES);
+    }
+    return $categories;
+  }
+  function getConfirmRegCheck(): bool {
+    if(htmlentities($_POST['confirm_reg_check'], ENT_QUOTES)=="CHECKED"){
+      return TRUE;
+    } else {
+      $this->flag = FALSE;
+      return FALSE;
+    }
+  }
+  function getRememberCheck(): bool {
+    return htmlentities($_POST['remember_check'], ENT_QUOTES) == "REMEMBER" ? TRUE : FALSE;
+  }
 }
 //
-echo "User Email: " . get_POST_email() . "<br>" . "User password(HASH): " . get_POST_password() . "<br>";
+$_post = new post_class();
+$hash=password_hash($_post->getPassword(), PASSWORD_DEFAULT);
+echo "User Email: " . $_post->getEmail() . "<br>" . "User password: " . $_post->getPassword() . "<br>________________________<br>";
+$hash2=password_hash($_post->getPassword(), PASSWORD_DEFAULT);
+echo $hash . "<br>" . $is_correct = password_verify($_post->getPassword(), $hash)? "password is correct<br>":"password failed<br>";
+echo $hash2 . "<br>" . $is_correct = password_verify($_post->getPassword(), $hash2)? "password is correct<br>":"password failed<br>";
+$cat = $_post->getCategories();
+echo $cat;
