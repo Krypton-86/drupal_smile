9c2aa85d750a71c7001391d507d3328a3fa02e10
Petro <mccolaichuk@gmail.com>
2021-09-07 00:07:18 +0300

    Removed sensitive information

--- PHP_BASICS/action_authorization.php
+++ PHP_BASICS/action_authorization.php
@@ -1,21 +1,12 @@
 <?php
 require 'post_class.php';
-// Sensitive information:
-$username = "user";
-$password = "password";
-$database = "users";
-$servername = "localhost";
-// Create connection
-$db_conn = mysqli_connect($servername, $username, $password);
-// Check connection
-if (!$db_conn) {
-  die("Connection failed: " . mysqli_connect_error());
-} else {
-  echo "Connected successfully<br>";
+require 'db_connect.php';
+
   // Prepare query, check validity
   $_post_auth = new post_class();
   if ($_post_auth->getValidStatus()) {
-    $db_query = "SELECT users.info.user_id, users.info.Email, users.hashes.hash_id, users.hashes.hash FROM users.info LEFT JOIN users.hashes ON users.info.user_id=users.hashes.hash_id WHERE users.info.Email = '" . $_post_auth->getEmail() . "';";
+    $db_query = "SELECT info.user_id, info.Email, hashes.hash_id, hashes.hash FROM users.info LEFT JOIN hashes ON info.user_id = hashes.hash_id WHERE Email='" . $_post_auth->getEmail() . "';";
+//    $db_query = "SELECT user_id FROM info;";
     $result = mysqli_query($db_conn, $db_query);
     // Run query for writing user info
     if (mysqli_num_rows($result) > 0) {
@@ -32,7 +23,7 @@ if (!$db_conn) {
     echo "<br>" . $_post_auth->getErrors() . "<br>";
   }
   mysqli_close($db_conn);
-}
+
 //$hash=password_hash($_post->getPassword(), PASSWORD_DEFAULT);
 //echo "User Email: " . $_post->getEmail() . "<br>" . "User password: " . $_post->getPassword() . "<br>________________________<br>";
 //$hash2=password_hash($_post->getPassword(), PASSWORD_DEFAULT);
