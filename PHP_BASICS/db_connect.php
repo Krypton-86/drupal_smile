d93d0190fa7b4618d8a1c2fac356ef318259ee21
Petro <mccolaichuk@gmail.com>
2021-09-07 00:05:35 +0300

    Moved db connect and password to this file

---
+++ PHP_BASICS/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+// Sensitive information:
+$username = "user";
+$password = "password";
+$database = "users";
+$servername = "localhost";
+// Create connection
+$db_conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$db_conn) {
+  die("Connection failed: " . mysqli_connect_error());
+} else {
+  echo "Connected successfully<br>";
+}
