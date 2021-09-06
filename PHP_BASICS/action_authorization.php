9fd2fdea5187a5b36dc875983c5c9e509a85f01c
Petro <mccolaichuk@gmail.com>
2021-09-06 18:23:41 +0300

    Not working authorization query #18

--- PHP_BASICS/action_authorization.php
+++ PHP_BASICS/action_authorization.php
@@ -1,8 +1,38 @@
 <?php
 require 'post_class.php';
-require 'variables.php';
-//
-$_post = new post_class();
+// Sensitive information:
+$username = "user";
+$password = "password";
+$database = "users";
+$servername = "localhost";
+// Create connection
+$db_conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$db_conn) {
+  die("Connection failed: " . mysqli_connect_error());
+} else {
+  echo "Connected successfully<br>";
+  // Prepare query, check validity
+  $_post_auth = new post_class();
+  if ($_post_auth->getValidStatus()) {
+    $db_query = "SELECT users.info.user_id, users.info.Email, users.hashes.hash_id, users.hashes.hash FROM users.info LEFT JOIN users.hashes ON users.info.user_id=users.hashes.hash_id WHERE users.info.Email = '" . $_post_auth->getEmail() . "';";
+    $result = mysqli_query($db_conn, $db_query);
+    // Run query for writing user info
+    if (mysqli_num_rows($result) > 0) {
+     echo "ok";
+      while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["user_id"] . " - Email: " . $row["Email"] . "<br>";
+      }
+    }
+    else {
+      echo "Error finding user info: " . $db_query . "<br>" . mysqli_error($db_conn);
+    }
+  } else {
+    //Write validity errors
+    echo "<br>" . $_post_auth->getErrors() . "<br>";
+  }
+  mysqli_close($db_conn);
+}
 //$hash=password_hash($_post->getPassword(), PASSWORD_DEFAULT);
 //echo "User Email: " . $_post->getEmail() . "<br>" . "User password: " . $_post->getPassword() . "<br>________________________<br>";
 //$hash2=password_hash($_post->getPassword(), PASSWORD_DEFAULT);
---
+++ PHP_BASICS/mypage.php
@@ -0,0 +1,6 @@
+<?php
+/*
+After successful login, the user should be redirected to “My Page”, where he can see data from the registration form.
+Make the user session alive after closing tab/browser (user is still logged-in).
+Add the possibility to edit user data on the My Profile page (/profile.php).
+*/
