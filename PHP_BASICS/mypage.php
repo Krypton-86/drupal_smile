0f2e7820f2f9270f53fe667d51c9881ff2187b26
Petro <mccolaichuk@gmail.com>
2021-09-06 18:26:05 +0300

    Created user pages

--- PHP_BASICS/mypage.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-After successful login, the user should be redirected to “My Page”, where he can see data from the registration form.
-Make the user session alive after closing tab/browser (user is still logged-in).
-Add the possibility to edit user data on the My Profile page (/profile.php).
-*/
--- PHP_BASICS/profile.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-Add the possibility to edit user data on the My Profile page (/profile.php).
-This data is the same as the user entered on the registration.
-Add validations to My Profile edit page as well (e.g. user can’t change email to already existing email of another user, etc.).
- */
