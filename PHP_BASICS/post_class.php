83374bfe7af34656afe7a7f57f8ce548548d1b21
Petro <mccolaichuk@gmail.com>
2021-09-07 00:17:11 +0300

    Working registration

--- PHP_BASICS/post_class.php
+++ PHP_BASICS/post_class.php
@@ -1,82 +1,100 @@
 <?php
 
 class post_class {
+
   private $isValid = TRUE;
+
   private $errors = [];
+
   function getEmail(): string {
     $return_var = htmlentities($_POST['email'], ENT_QUOTES);
-    $len=strlen($return_var);
-    if ($len < 5 || $len > 255 || !filter_var($return_var, FILTER_VALIDATE_EMAIL)){
+    $len = strlen($return_var);
+    if ($len < 5 || $len > 255 || !filter_var($return_var, FILTER_VALIDATE_EMAIL)) {
       $this->isValid = FALSE;
       array_push($this->errors, "Email: Enter a valid email.");
     }
     return $return_var;
   }
+
   function getPassword(): string {
     $return_var = htmlentities($_POST['password'], ENT_QUOTES);
-    $len=strlen($return_var);
-    if ($len < 8 || $len > 32){
-      $this->isValid=FALSE;
+    $len = strlen($return_var);
+    if ($len < 8 || $len > 32) {
+      $this->isValid = FALSE;
       array_push($this->errors, "Password: Use passphrase from 8 to 32 symbols.");
     }
     return $return_var;
   }
+
   function getPasswordHash(): string {
     return password_hash($this->getPassword(), PASSWORD_DEFAULT);
   }
+
   function getFname(): string {
     $return_var = $_POST['fname'];
-    $len=strlen($return_var);
-    if ($len < 2 || $len > 255 || !preg_match("/^[a-zA-Z-' ]*$/",$return_var)){
-      $this->isValid=FALSE;
+    $len = strlen($return_var);
+    if ($len < 2 || $len > 255 || !preg_match("/^[a-zA-Z-' ]*$/", $return_var)) {
+      $this->isValid = FALSE;
       array_push($this->errors, "First name: Only letters and white space allowed, must be longer than one symbol.");
     }
     return htmlentities($return_var, ENT_QUOTES);
   }
+
   function getLname(): string {
     $return_var = $_POST['lname'];
-    $len=strlen($return_var);
-    if ($len < 2 || $len > 255 || !preg_match("/^[a-zA-Z-' ]*$/",$return_var)){
-      $this->isValid=FALSE;
+    $len = strlen($return_var);
+    if ($len < 2 || $len > 255 || !preg_match("/^[a-zA-Z-' ]*$/", $return_var)) {
+      $this->isValid = FALSE;
       array_push($this->errors, "Last name: Only letters and white space allowed, must be longer than one symbol.");
     }
     return htmlentities($return_var, ENT_QUOTES);
   }
+
   function getBirthday(): string {
     $return_var = htmlentities($_POST['birthday'], ENT_QUOTES);
-    $len=strlen($return_var);
-    if ($len < 8 || $len > 10 || !preg_match("/^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$/",$return_var)){
+    $len = strlen($return_var);
+    if ($len < 8 || $len > 10 || !preg_match("/^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$/", $return_var)) {
       $this->isValid = FALSE;
       array_push($this->errors, "Birthdate: Check date.");
     }
     return $return_var;
   }
+
+  //Todo:Change output to array
   function getCategories(): string {
     $categories = NULL;
-    foreach ($_POST['categories'] as $category){
-      if ($categories===NULL){
+    foreach ($_POST['categories'] as $category) {
+      if ($categories === NULL) {
         $categories = $category;
-      }else
+      }
+      else {
         $categories = $categories . ", " . htmlentities($category, ENT_QUOTES);
+      }
     }
     return $categories;
   }
+
   function getConfirmRegCheck(): bool {
-    if(htmlentities($_POST['confirm_reg_check'], ENT_QUOTES)=="CHECKED"){
+    if (htmlentities($_POST['confirm_reg_check'], ENT_QUOTES) == "CHECKED") {
       return TRUE;
-    } else {
+    }
+    else {
       $this->isValid = FALSE;
       array_push($this->errors, "Confirm registration: User not confirmed registration!");
       return FALSE;
     }
   }
+
   function getRememberCheck(): bool {
     return htmlentities($_POST['remember_check'], ENT_QUOTES) == "REMEMBER";
   }
-  function getValidStatus(): bool{
+
+  function getValidStatus(): bool {
     return $this->isValid;
   }
-  function getErrors(): array{
+
+  function getErrors(): array {
     return $this->errors;
   }
+
 }
--- PHP_BASICS/variables.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysql_username = "user";
-$mysql_password = "password";
-$mysql_database = "users";
-$mysql_servername = "localhost";
