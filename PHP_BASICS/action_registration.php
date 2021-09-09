b1f5fd1e6dcc2fdd154c3b2714ed18f51730b5c9
Petro <mccolaichuk@gmail.com>
2021-09-09 16:40:05 +0300

    Basicaly changed architecture to OOP

--- PHP_BASICS/action_registration.php
+++ PHP_BASICS/action_registration.php
@@ -5,52 +5,11 @@ if (array_key_exists('user_id', $_COOKIE)) {
 }
 require 'post_class.php';
 require 'db_connect.php';
+require 'user.php';
 
-$_post_reg = new post_class();
-$is_registered = TRUE;
-// Check email is used
-$db_query_email = "SELECT user_id FROM smile.users WHERE Email='" . $_post_reg->getEmail() . "';";
-$result = mysqli_query($db_conn, $db_query_email);
-if (mysqli_num_rows($result) > 0) {
-  $is_registered = TRUE;
-}
-else {
-  $is_registered = FALSE;
-}
-if ($_post_reg->getValidStatus() && $_post_reg->getConfirmRegCheck() && !$is_registered) {
-  $db_query = "INSERT INTO smile.users (First_name, Last_name, Email, Birthday, Password" . $_post_reg->getCategoriesString() . ")
-  VALUES ('" . $_post_reg->getFname()
-    . "', '" . $_post_reg->getLname()
-    . "', '" . $_post_reg->getEmail()
-    . "', '" . $_post_reg->getBirthday()
-    . "', '" . $_post_reg->getPasswordHash()
-    . "'" . $_post_reg->getCategoriesTruesMap()
-    . ");";
-  // Run query for writing user info
-  if (mysqli_query($db_conn, $db_query)) {
-    $result = mysqli_query($db_conn, $db_query_email);
-    if (mysqli_num_rows($result) > 0) {
-      $row = mysqli_fetch_assoc($result);
-      setcookie("user_id", $row['user_id'], time() + (86400 * 30), "/"); // 86400 = 1 day
-      echo '<style> h1, h2{text-align: center; color: darkslategrey;}</style> <br><br><br><h1>New user info record created successfully!<h1><br>';
-      echo "Dear " . $_post_reg->getFname() . ", <br><h2>Next time you can authorize with email in this page: <br><a href='/index.php'>Log In</a><h2>";
-      header("refresh:5;url=mypage.php");
-    }
-  }
-  else {
-    echo "Error writing user info: " . $db_query . "<br>" . mysqli_error($db_conn);
-  }
-}
-else {
-  //Write validity errors
-  if ($is_registered) {
-    echo '<style> h1, h2{text-align: center; color: firebrick;}</style> <br><br><br><h1>This email registered!<h1><br>';
-  }
-  else {
-    echo implode("<br>", $_post_reg->getErrors());
-  }
-  header("refresh:3;url=registration.html");
-}
-mysqli_close($db_conn);
+$_post = new post_class();
+$user = new user();
+$user->register($db_conn, $_post);
+//mysqli_close($db_conn);
 
 
--- PHP_BASICS/db_connect.php
+++ PHP_BASICS/db_connect.php
@@ -1,15 +1,22 @@
 <?php
-// Sensitive information:
+//class db {
+  // Sensitive information:
 $username = "user";
 $password = "password";
 $database = "users";
 $servername = "localhost";
-// Create connection
-$db_conn = mysqli_connect($servername, $username, $password);
-// Check connection
-if (!$db_conn) {
-  die("<style> h6{text-align: left; color: firebrick}</style><h6>Connection failed: </h6>>" . mysqli_connect_error());
-}
-else {
-//  echo "<style> h6{text-align: left;}</style><h6>Connected successfully</h6><br>";
-}
+//function connect () {
+  $db_conn = mysqli_connect($servername, $username, $password);
+  // Check connection
+  if (!$db_conn) {
+
+    die("<style> h6{text-align: left; color: firebrick}</style><h6>Connection failed: </h6>>" . mysqli_connect_error());
+
+  }
+
+  else {
+    //  echo "<style> h6{text-align: left;}</style><h6>Connected successfully</h6><br>";
+    return $db_conn;
+  }
+//}
+//}
