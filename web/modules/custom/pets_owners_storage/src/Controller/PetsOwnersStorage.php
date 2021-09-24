a3548e6b2222980906ed5bf50f701db427269f01
wodby <wodby@example.com>
2021-09-24 14:22:20 +0000

    Working deleting db data

--- web/modules/custom/pets_owners_storage/src/Controller/PetsOwnersStorage.php
+++ web/modules/custom/pets_owners_storage/src/Controller/PetsOwnersStorage.php
@@ -2,9 +2,95 @@
 
 namespace Drupal\pets_owners_storage\Controller;
 
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
+
 /**
  * Class to store data from pets_owners_form.
  */
 class PetsOwnersStorage {
+
+  /**
+   * Implements access to all pets owners.
+   */
+  public function access() {
+    $user = \Drupal::currentUser();
+    if ($user->id() == 1 && $user->hasPermission('access content')) {
+      return AccessResult::allowed();
+    }
+    else {
+      return AccessResult::forbidden();
+    }
+  }
+
+  /**
+   * Implements database info output.
+   */
+  public function getInfo($start = 0, $length = 10) {
+    $result  = \Drupal::database()
+      ->select('pets_owners_storage', 'st')
+      ->fields('st', [
+        'id',
+        'name',
+        'gender',
+        'prefix',
+        'age',
+        'mother_name',
+        'father_name',
+        'have_pets',
+        'pets_name',
+        'email',
+      ])
+      ->range($start, $length)
+      ->orderBy('id', 'DESC')
+      ->execute()
+      ->fetchAll();
+    $r       = 0;
+    $h       = 0;
+    $v       = 0;
+    $rows    = [];
+    $headers = [];
+    foreach ($result as $array) {
+      foreach ($array as $key => $value) {
+        if ($h < 10) {
+          $headers[$h] = $key;
+          $h++;
+        }
+        $rows[$r][$v] = $value;
+        $v++;
+      }
+      $rows[$r][$v] = Link::fromTextAndUrl('Delete', Url::fromUserInput("/pets_owners/delete/" . $rows[$r][0]));
+      $v = 0;
+      $r++;
+    }
+    $content['table'] = [
+      '#type' => 'table',
+      '#header' => $headers,
+      '#rows' => $rows,
+      '#empty' => '',
+    ];
+    return $content;
+  }
+
+  /**
+   * Implements editing information in database.
+   */
+  public function editInfo($id, $field, $value) {
+
+  }
+
+  /**
+   * Implements deleting info from database.
+   */
+  public function deleteInfo($id) {
+    $result = \Drupal::database()
+      ->delete('pets_owners_storage')
+      ->condition('id', $id)
+      ->execute();
+    return [
+      '#markup' => '<p>' . "User data with id:" . ("$id") . " successfully deleted ($result record affected)" . '</p>',
+    ];
+  }
 
 }
