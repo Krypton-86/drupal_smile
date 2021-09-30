cc9d6358d5c4861ca69257d3518d49eed0d69537
Petro <mccolaichuk@gmail.com>
2021-09-30 20:56:30 +0300

    Described  fieldType

---
+++ web/modules/custom/youtube_plugin/src/Plugin/Field/FieldType/YoutubeItem.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\youtube_plugin\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Plugin implementation of the Youtube field type.
+ *
+ * @FieldType(
+ *   id = "youtube_plugin",
+ *   label = @Translation("Youtube video"),
+ *   module = "youtube_plugin",
+ *   description = @Translation("Output video from Youtube."),
+ *   default_widget = "youtube_plugin",
+ *   default_formatter = "youtube_plugin"
+ * )
+ */
+class YoutubeItem extends FieldItemBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    return array(
+      'columns' => array(
+        'value' => array(
+          'type' => 'text',
+          'size' => 'tiny',
+          'not null' => FALSE,
+        ),
+      ),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $value = $this->get('value')->getValue();
+    return $value === NULL || $value === '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(t('Youtube video URL'));
+
+    return $properties;
+  }
+
+}
