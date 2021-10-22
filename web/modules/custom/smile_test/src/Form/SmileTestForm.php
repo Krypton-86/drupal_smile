698d6f0eb06b60336416d8d3f41b050e9851a85e
Petro <mccolaichuk@gmail.com>
2021-10-22 16:41:01 +0300

    Replaced own code by Roman better

---
+++ web/modules/custom/smile_test/src/Form/SmileTestForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\smile_test\Form;
+
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Language\Language;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Language\LanguageInterface;
+
+/**
+ *
+ */
+class SmileTestForm extends ContentEntityForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    /* @var $entity \Drupal\smile_test\Entity\SmileTest */
+    $form = parent::buildForm($form, $form_state);
+    $entity = $this->entity;
+
+    $form['langcode'] = [
+      '#title' => $this->t('Language'),
+      '#type' => 'language_select',
+      '#default_value' => $entity->getUntranslated()->language()->getId(),
+      '#languages' => LanguageInterface::STATE_ALL,
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $form_state->setRedirect('entity.smile_test.collection');
+    $entity = $this->getEntity();
+    $entity->save();
+  }
+
+}
