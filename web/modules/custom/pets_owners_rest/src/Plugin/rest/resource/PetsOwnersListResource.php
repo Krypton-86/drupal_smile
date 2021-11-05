11373d9bd66222f8e551772aaff068a26ea5c118
Petro <mccolaichuk@gmail.com>
2021-11-05 18:29:05 +0200

    Added age condition

--- web/modules/custom/pets_owners_rest/src/Plugin/rest/resource/PetsOwnersListResource.php
+++ web/modules/custom/pets_owners_rest/src/Plugin/rest/resource/PetsOwnersListResource.php
@@ -33,15 +33,15 @@ class PetsOwnersListResource extends ResourceBase {
    */
   public function get() {
     $page = \Drupal::request()->get('page') <= 0 ? 1 : \Drupal::request()->get('page');
-    $limit = \Drupal::request()->get('limit') <= 0 ? 1 : \Drupal::request()->get('limit');
+    $limit = \Drupal::request()->get('limit') <= 0 ? 2 : \Drupal::request()->get('limit');
+    $age = \Drupal::request()->get('age');
 
-    $name = \Drupal::request()->get('name') . '%';
     $result = \Drupal::database()
       ->select('pets_owners_storage', 'st')
       ->fields('st')
       ->orderBy('st.id', 'ASC');
-    if (!empty($name)) {
-      $result = $result->condition('st.name', $name, 'LIKE');
+    if (!empty($age)) {
+      $result = $result->condition('st.age', $age);
     }
     $result = $result->range(($page - 1) * $limit, $limit)
       ->execute()->fetchAllAssoc('id');
